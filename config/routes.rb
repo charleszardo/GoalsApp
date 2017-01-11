b1c14be0984c84068811e68b4bb44b0f9d2ea16a
Dave Charnuska <davecharnuska@gmail.com>
2017-01-11 00:23:33 -0500

    add session route, tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "goals#index"
 
   resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create]
 
   resources :goals, only: [:index]
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe "GET #new" do
+    it "renders the new session page" do
+      get :new, user: {}
 
+      expect(response).to render_template("new")
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST #create" do
+    context "with invalid params" do
+      it "validates the presence of title and body" do
+        post :create, user: {username: "invalid_user"}
+        expect(response).to render_template("new")
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+    context "with valid params" do
+      it "redirects to the goals index page" do
+        post :create, user: {username: "valid_user", password: "abcdef"}
+        expect(response).to redirect_to(goals_url)
+      end
+    end
+  end
 end
