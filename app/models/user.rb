591ac380ddc39de05d48a5ccf5009ada91b62955
Dave Charnuska <davecharnuska@gmail.com>
2017-01-10 22:25:25 -0500

    add more user model auth-related specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,25 @@ class User < ActiveRecord::Base
 
   after_initialize :ensure_session_token
 
+  def self.find_by_username_and_password(username, password)
+    user = User.find_by_username(username)
+    user && user.is_password?(password) ? user : nil
+  end
+
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
     self.save
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
   private
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,25 @@ RSpec.describe User, :type => :model do
       expect(BCrypt::Password).to receive(:create)
       User.new(username: "test_user", password: "abcdef")
     end
+
+    describe "#is_password?" do
+      it "correctly determines if password belongs to user" do
+        expect(user.is_password?(user.password)).to be true
+        expect(user.is_password?("invalid_password")).to be false
+      end
+    end
+  end
+
+  describe "::find_by_username_and_password" do
+    it "returns user when credentials are valid" do
+      user = User.create!(username: "test_user", password: "abcdef")
+      expect(User.find_by_username_and_password(user.username, user.password)).to eq(user)
+    end
+
+    it "returns nil when credentials are invalid" do
+      user = User.create!(username: "test_user", password: "abcdef")
+      expect(User.find_by_username_and_password(user.username, "invalid_password")).to be_nil
+    end
   end
 
   describe "session_token" do
@@ -23,10 +42,20 @@ RSpec.describe User, :type => :model do
       expect(user).to be_valid
       expect(user.session_token).to_not be_nil
     end
+
+    it "resets session token" do
+      original_token = user.session_token
+      new_token = user.reset_session_token!
+      expect(original_token).to_not eq(new_token)
+      expect(user.session_token).to eq(new_token)
+    end
   end
 
   it { should validate_presence_of(:username) }
   it { should validate_uniqueness_of(:username) }
   it { should validate_length_of(:password).is_at_least(6) }
+  it { should validate_presence_of(:session_token) }
+  it { should validate_uniqueness_of(:session_token) }
+  it { should validate_presence_of(:password_digest) }
   # it { should have_many(:goals) }
 end
