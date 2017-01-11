24fe4de48c17d85bfbe16620b8d0e993a4bc7e2c
Dave Charnuska <davecharnuska@gmail.com>
2017-01-11 16:31:27 -0500

    add more auth specs

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -20,7 +20,7 @@ feature "the signup process" do
       expect(page).to have_content "valid_username"
     end
 
-    scenario "redirects users show page after sign up" do
+    scenario "redirects to users show page after sign up" do
       expect(page).to have_content "My Goals"
     end
 
@@ -29,15 +29,40 @@ feature "the signup process" do
 end
 
 feature "logging in" do
+  before(:each) do
+    visit new_user_url
+    fill_in "Username", with: "valid_username"
+    fill_in "Password", with: "valid_password"
+    click_on "submit"
+    click_on "Logout"
+  end
 
-  scenario "shows username on the homepage after login"
+  scenario "shows username on the homepage after login" do
+    visit new_session_url
+    fill_in "Username", with: "valid_username"
+    fill_in "Password", with: "valid_password"
+    click_on "submit"
+    expect(page).to have_content "valid_username"
+  end
 
 end
 
 feature "logging out" do
 
-  scenario "begins with a logged out state"
+  scenario "begins with a logged out state" do
+    visit goals_url
+    expect(page).to have_content "Sign Up"
+    expect(page).to have_content "Sign In"
+    expect(page).to_not have_content "Welcome"
+  end
 
-  scenario "doesn't show username on the homepage after logout"
+  scenario "doesn't show username on the homepage after logout" do
+    visit new_user_url
+    fill_in "Username", with: "valid_username"
+    fill_in "Password", with: "valid_password"
+    click_on "submit"
+    click_on "Logout"
+    expect(page).to_not have_content "valid_username"
+  end
 
 end
