08fd53cede5553f0c51c0e1d6759bdd12904cbee
Dave Charnuska <davecharnuska@gmail.com>
2017-01-11 16:09:01 -0500

    begin adding auth_spec

---
+++ spec/features/auth_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature "the signup process" do
+
+  scenario "has a new user page" do
+    visit new_user_url
+    expect(page).to have_content "Sign Up"
+  end
+
+  feature "signing up a user" do
+    before(:each) do
+      visit new_user_url
+      fill_in "Username", with: "valid_username"
+      fill_in "Password", with: "valid_password"
+      click_on "submit"
+    end
+
+    scenario "shows username on the homepage after signup" do
+      expect(page).to have_content "valid_username"
+    end
+
+    scenario "redirects users show page after sign up" do
+      expect(page).to have_content "My Goals"
+    end
+
+  end
+
+end
+
+feature "logging in" do
+
+  scenario "shows username on the homepage after login"
+
+end
+
+feature "logging out" do
+
+  scenario "begins with a logged out state"
+
+  scenario "doesn't show username on the homepage after logout"
+
+end
