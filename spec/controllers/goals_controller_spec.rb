313d79b763551891145b83b0608d1c326cd42756
Dave Charnuska <davecharnuska@gmail.com>
2017-01-11 16:43:40 -0500

    remove goals controller index spec

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe GoalsController, type: :controller do
-  describe "GET #index" do
-    it "renders the goals index page" do
-      get :index
 
-      expect(response).to render_template("index")
-    end
-  end
 end
