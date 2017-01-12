a7987f35779e014baf28d0e98b0c2ae97797f30c
Dave Charnuska <davecharnuska@gmail.com>
2017-01-12 14:44:50 -0500

    move some cheers controller functionality to model

--- app/controllers/cheers_controller.rb
+++ app/controllers/cheers_controller.rb
@@ -1,17 +1,12 @@
 class CheersController < ApplicationController
   def create
-    if current_user.has_remaining_cheers?
-      Cheer.create!(user: current_user, goal: params[:goal_id])
-    end
+    Cheer.create_cheers(current_user, Goal.find(params[:goal_id]))
 
     redirect_to goal_url(params[:goal_id])
   end
 
   def destroy
-    if current_user.has_cheersed_goal?(params[:goal_id])
-      cheer = Cheer.find_by_user_and_goal(user: current_user, goal: params[:goal_id])
-      cheer.destroy!
-    end
+    Cheer.destroy_cheers(current_user, Goal.find(params[:goal_id]))
 
     redirect_to goal_url(params[:goal_id])
   end
--- app/models/cheer.rb
+++ app/models/cheer.rb
@@ -3,4 +3,18 @@ class Cheer < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :goal
+
+  def self.create_cheers(user, goal)
+    if user.has_remaining_cheers?
+      Cheer.create!(user: current_user, goal: goal)
+      user.cheers_goal
+    end
+  end
+
+  def self.destroy_cheers(user, goal)
+    if user.has_cheersed_goal?(goal)
+      cheer = Cheer.find_by_user_and_goal(user, goal)
+      cheer.destroy! if cheer
+    end
+  end
 end
