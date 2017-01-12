5c5430e9c87e5baf063864dd98d4729b164fc87b
Dave Charnuska <davecharnuska@gmail.com>
2017-01-12 02:47:51 -0500

    add goal comment table, model, associations, validations

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,7 @@
 class Goal < ActiveRecord::Base
   validates :user, :title, presence: true
   validates :public, :complete, inclusion: { in: [ true, false ] }
-  
+
   belongs_to :user
+  has_many :comments, class_name: "GoalComment"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ActiveRecord::Base
 
   has_many :goals
   has_many :comments, class_name: "UserComment"
-  has_many :authored_comments, class_name: "UserComment", foreign_key: :author_id
+  has_many :authored_user_comments, class_name: "UserComment", foreign_key: :author_id
+  has_many :authored_goal_comments, class_name: "UserComment", foreign_key: :author_id
 
   def self.find_by_username_and_password(username, password)
     user = User.find_by_username(username)
