c728ed754fa959754dfdf184936f3d5cb22e9fa7
Dave Charnuska <davecharnuska@gmail.com>
2017-01-12 12:09:55 -0500

    add comments table

---
+++ db/migrate/20170112165432_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.integer :author_id, null: false
+      t.text :body, null: false
+      t.references :commentable, polymorphic: true, index: true
+
+      t.timestamps null: false
+    end
+
+    add_index :comments, :author_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112074507) do
+ActiveRecord::Schema.define(version: 20170112165432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "author_id",        null: false
+    t.text     "body",             null: false
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "comments", ["author_id"], name: "index_comments_on_author_id", using: :btree
+  add_index "comments", ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id", using: :btree
 
   create_table "goal_comments", force: :cascade do |t|
     t.integer  "author_id",  null: false
