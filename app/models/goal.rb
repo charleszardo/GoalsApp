5d7f2a64a819efe07de9f25db415170f3eeecce1
Dave Charnuska <davecharnuska@gmail.com>
2017-01-12 17:09:20 -0500

    add #cheers_count

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -7,20 +7,15 @@ class Goal < ActiveRecord::Base
   belongs_to :user
   has_many :cheers
 
-  def cheers_count
-    Cheer.where(goal: self).count
-  end
-
   def self.top_goals
-    # Goal.select("goal.*, COUNT(cheers.id) as cheers_count")
-    #     .joins(:cheers)
-    #     .group(:goals)
-    #     .limit(10)
-
     Goal.select("goals.*, COUNT(cheers.id) as cheers_count")
         .joins(:cheers)
         .group("goals.id")
         .limit(10)
         .order("cheers_count DESC")
+  end
+
+  def cheers_count
+    Cheer.where(goal: self).count
   end
 end
